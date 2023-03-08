a1e198f8d07b100d651977677f87ad44e2a69208
Mausam Rai <mausamrai11@gmail.com>
2023-03-07 23:08:06 -0500

    second commit

---
+++ src/main/java/com/jpai/jpai/Try.java
@@ -0,0 +1,13 @@
+package com.jpai.jpai;
+
+public class Try {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Welcome to the world of java where objects play.");
+		
+		
+	}
+
+}
